86361349d6df00fc9e3c9efafd6cf4edfac1f02b
Kiselev SE <semyon.k@sunrise-r.ru>
2020-08-29 15:53:59 +0300

    Initial version of kiptender generated by JHipster-6.9.0 with blueprints: vuejs-1.8.1

---
+++ src/main/java/com/ingen/sky/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.ingen.sky.service.dto;
---
+++ src/test/java/com/ingen/sky/ArchTest.java
@@ -0,0 +1,29 @@
+package com.ingen.sky;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.ingen.sky");
+
+        noClasses()
+            .that()
+                .resideInAnyPackage("com.ingen.sky.service..")
+            .or()
+                .resideInAnyPackage("com.ingen.sky.repository..")
+            .should().dependOnClassesThat()
+                .resideInAnyPackage("..com.ingen.sky.web..")
+        .because("Services and repositories should not depend on web layer")
+        .check(importedClasses);
+    }
+}
